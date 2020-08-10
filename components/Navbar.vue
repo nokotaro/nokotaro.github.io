ef0f34ab9f9599d771c3d440c34770a29b037e45
nokotaro <takenoko@mail.nokotaro.com>
2020-08-10 10:35:26 +0900

    fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,34 @@
 <template lang="pug">
 div
   header
-    nav.navbar.fixed-top.navbar-expand-sm.bg-dark.navbar-dark
-      nuxt-link.navbar-brand(to="/")
-        img(src="/images/icon.png", width="30", height="30", alt="")
-        |
-        | Portfolio
-      ul.navbar-nav.mr-auto
-        li.nav-item
-          nuxt-link(
-            v-bind:class="[now === 'top' ? 'nav-link active' : 'nav-link']",
-            to="/"
-          ) Top
-        li.nav-item
-          nuxt-link(
-            v-bind:class="[now === 'about' ? 'nav-link active' : 'nav-link']",
-            to="/about"
-          ) About
-        li.nav-item
-          nuxt-link(
-            v-bind:class="[now === 'project' ? 'nav-link active' : 'nav-link']",
-            to="/project"
-          ) Project
+    nav.navbar.fixed-top.navbar-expand.navbar-dark.bg-dark
+      .container-fluid
+        nuxt-link.navbar-brand.mb-0.h1(to="/")
+          img.d-inline-block.align-top(
+            src="/images/icon.png",
+            width="30",
+            height="30",
+            alt="",
+            loading="lazy"
+          )
+          |
+          | Portfolio
+        ul.navbar-nav.mr-auto
+          li.nav-item
+            nuxt-link(
+              v-bind:class="[now === 'top' ? 'nav-link active' : 'nav-link']",
+              to="/"
+            ) Top
+          li.nav-item
+            nuxt-link(
+              v-bind:class="[now === 'about' ? 'nav-link active' : 'nav-link']",
+              to="/about"
+            ) About
+          li.nav-item
+            nuxt-link(
+              v-bind:class="[now === 'project' ? 'nav-link active' : 'nav-link']",
+              to="/project"
+            ) Project
 </template>
 
 <script lang="ts">
